940068d8c26a2e03fbebaa21e6f6ebab40ae3568
Shelby Shook <shelby.shook1217@gmail.com>
2023-06-01 11:27:48 -0400

    Establishing User Table

--- ElevenNote.Data/ApplicationDbContext.cs
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ElevenNote.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -13,5 +14,7 @@ namespace ElevenNote.Data
             : base(options)
         {
         }
+
+        public DbSet<UserEntity> Users { get; set; }
     }
 }
